a6ab30d869a388c5f5fbbba1088c2c90f913f56d
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-02-04 22:36:35 +0000

    Add spawn point id to field module args

--- Runtime/SharedTypes.cs
+++ Runtime/SharedTypes.cs
@@ -5,18 +5,22 @@ namespace RPGFramework.Field.SharedTypes
 {
     public interface IFieldModuleArgs : IModuleArgs
     {
+        int    GetSpawnId { get; }
         string GetFieldId { get; }
     }
 
     public readonly struct FieldModuleArgs : IFieldModuleArgs
     {
+        public int    GetSpawnId => m_SpawnId;
         public string GetFieldId => m_FieldId;
 
-        private readonly string m_FieldId;
+        public readonly int    m_SpawnId;
+        public readonly string m_FieldId;
 
-        public FieldModuleArgs(string fieldId)
+        public FieldModuleArgs(string fieldId, int spawnId)
         {
             m_FieldId = fieldId;
+            m_SpawnId = spawnId;
         }
     }
 
