45dcf442776b5881655997c9054611cebab75ea9
Elliott Armstrong <earmstrong1990@hotmail.co.uk>
2026-05-16 20:14:35 +0100

    Tweaks to how modules resume

--- Runtime/SharedTypes.cs
+++ Runtime/SharedTypes.cs
@@ -1,26 +1,40 @@
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using RPGFramework.Core.SharedTypes;
+using Unity.Mathematics;
 
 namespace RPGFramework.Field.SharedTypes
 {
     public interface IFieldModuleArgs : IModuleArgs
     {
-        string   GetFieldId               { get; }
-        int      GetSpawnId               { get; }
-        string[] LocalisationSheetsToLoad { get; }
+        int        FieldId  { get; }
+        int        SpawnId  { get; }
+        float3     Position { get; }
+        quaternion Rotation { get; }
     }
 
+    [StructLayout(LayoutKind.Sequential)]
     public readonly struct FieldModuleArgs : IFieldModuleArgs
     {
-        public string   GetFieldId               { get; }
-        public int      GetSpawnId               { get; }
-        public string[] LocalisationSheetsToLoad { get; }
+        public int        FieldId  { get; }
+        public int        SpawnId  { get; }
+        public float3     Position { get; }
+        public quaternion Rotation { get; }
 
-        public FieldModuleArgs(string fieldId, int spawnId, string[] localisationSheetsToLoad)
+        public FieldModuleArgs(int fieldId, int spawnId)
         {
-            GetFieldId               = fieldId;
-            GetSpawnId               = spawnId;
-            LocalisationSheetsToLoad = localisationSheetsToLoad;
+            FieldId  = fieldId;
+            SpawnId  = spawnId;
+            Position = float3.zero;
+            Rotation = quaternion.identity;
+        }
+
+        public FieldModuleArgs(int fieldId, int spawnId, float3 position, quaternion rotation)
+        {
+            FieldId  = fieldId;
+            SpawnId  = spawnId;
+            Position = position;
+            Rotation = rotation;
         }
     }
 
