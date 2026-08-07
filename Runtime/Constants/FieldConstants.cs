da5288ac117e63dc98f5e22d2201b45799fbb400
Elliott Armstrong <KandyMan1990@users.noreply.github.com>
2026-08-07 20:54:33 +0100

    Add module ID constant and spawn resume constant

---
+++ Runtime/Constants/FieldConstants.cs
@@ -0,0 +1,8 @@
+namespace RPGFramework.Field.SharedTypes.Constants
+{
+    public static class FieldConstants
+    {
+        public const byte MODULE_ID    = 0;
+        public const int  SPAWN_RESUME = -1;
+    }
+}
