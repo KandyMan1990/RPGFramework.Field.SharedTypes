350547e167d99ea778fe028ccd862149bb8f1223
Elliott Armstrong <KandyMan1990@users.noreply.github.com>
2026-08-09 15:53:50 +0100

    Remove unnecessary properties and methods

--- Runtime/Constants/FieldConstants.cs
+++ Runtime/Constants/FieldConstants.cs
@@ -2,7 +2,6 @@ namespace RPGFramework.Field.SharedTypes.Constants
 {
     public static class FieldConstants
     {
-        public const byte MODULE_ID    = 0;
-        public const int  SPAWN_RESUME = -1;
+        public const byte MODULE_ID = 0;
     }
 }
--- Runtime/SharedTypes.cs
+++ Runtime/SharedTypes.cs
@@ -4,6 +4,5 @@ namespace RPGFramework.Field.SharedTypes
 {
     public interface IFieldModule : IModule
     {
-        void RequestMenuModule(byte menuId);
     }
 }
